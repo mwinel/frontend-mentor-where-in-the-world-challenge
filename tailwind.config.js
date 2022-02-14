972bc04bee8ebb6c1fa0ed9067eefbcaabdf59d5
Nelson M <mwinel@live.com>
2022-02-14 21:46:01 +0300

    Add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
     darkMode: 'class',
@@ -9,36 +8,9 @@ module.exports = {
     ],
     theme: {
         typography: (theme) => ({}),
-        fontSize: {
-            'body-base': ['18px', '24px'],
-            xl: ['40px', '48px'],
-        },
-        letterSpacing: {
-            widest: '.35em',
-        },
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            red: colors.red,
-            neutral: '#FFFFFF',
-            'bright-blue': '#3A7BFD',
-            'check-bg-one': '#57DDFF',
-            'check-bg-two': '#C058f3',
-            // Light theme colors
-            'very-light-gray': '#FAFAFA',
-            'very-light-grayish-blue': '#E4E5F1',
-            'dark-grayish-blue': '#D2D3DB',
-            'very-dark-grayish-blue': '#484B6A',
-            // Dark theme colors
-            'very-dark-blue': '#161722',
-            'very-dark-desaturated-blue': '#25273C',
-            'light-grayish-blue': '#CACDE8',
-            'very-light-grayish-blue': '#E4E5F1',
-            'dark-grayish-blue': '#777A92',
-        },
         extend: {
             fontFamily: {
-                josefin: ['Josefin Sans', ...fontFamily.sans],
+                nunito: ['Nunito', ...fontFamily.sans],
             },
             display: ['group-hover'],
         },
