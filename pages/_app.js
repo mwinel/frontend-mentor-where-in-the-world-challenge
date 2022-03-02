061696becb6f1c0fcb2c0ed8e65580b5dd1d3c4a
Nelson M <mwinel@live.com>
2022-03-02 17:25:57 +0300

    Add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css';
+import Router from "next/router";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 import { ThemeProvider } from 'next-themes';
+
+// Binding events for loading indicator.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
     return (
